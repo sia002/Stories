22093605ac43c6e1ec909998e3a7be7be48a4af6
sia002 <158562949+sia002@users.noreply.github.com>
2024-10-29 11:01:32 +0100

    Add files via upload

--- init.php
+++ init.php
@@ -4,27 +4,28 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING & ~E_DEPRECATED & ~E_STRICT);
 
-define("CONFIG_LIVE", "0"); // 0: Test environment || 1: Live environment || 2: Docker
+define("CONFIG_LIVE", "0");
 
-if (CONFIG_LIVE == 0) {
+if (CONFIG_LIVE == 1) {
     $DB_SERVER = "localhost";
     $DB_NAME = "Stories";
     $DB_USER = "root";
     $DB_PASS = "root";
-} else if (CONFIG_LIVE == 1) {
+} else if (CONFIG_LIVE == 2) {
     $DB_SERVER = "";
     $DB_NAME = "";
     $DB_USER = "";
     $DB_PASS = "";
-} else if (CONFIG_LIVE == 2) {
+} else if (CONFIG_LIVE == ) {
     $DB_SERVER = "mariadb-standard";
     $DB_NAME = "webshop";
     $DB_USER = "user";
     $DB_PASS = "secretPassword";
 }
 
-// Opretter en instans af db-klassen
 $db = new db($DB_SERVER, $DB_NAME, $DB_USER, $DB_PASS);
+
+?>
 
 
 
